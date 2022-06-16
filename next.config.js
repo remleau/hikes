cbd2b48a1c8dc35c5450ef672d66573a93175fad
Remleau <remleau@gmail.com>
2022-06-16 00:45:09 -0400

    env index

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
-const path = require('path')
-const withSass = require('@zeit/next-sass');
-const { i18n } = require('./next-i18next.config');
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+const { i18n } = require("./next-i18next.config");
 
 module.exports = withSass({
   /* bydefault config  option Read For More Optios
   here https://github.com/vercel/next-plugins/tree/master/packages/next-sass*/
-  cssModules: true
+  cssModules: true,
 });
 
 module.exports = {
   /* Add Your Scss File Folder Path Here */
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
-}
+};
 
 module.exports = {
   i18n,
   env: {
-    apiKey: "AIzaSyAAkH9epxJqRA_DAZSDoRebXLLmmrsn1AE",
-    authDomain: "mountains-app-dev.firebaseapp.com",
-    projectId: "mountains-app-dev",
-    storageBucket: "mountains-app-dev.appspot.com",
-    messagingSenderId: "371191343534",
-    appId: "1:371191343534:web:d4e0c1e258d7abfe45003f",
-    googleMapsKey: "AIzaSyAl4X8CgLp0SWs4F5LoV9zgk34E7q9IHLg"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    googleMapsKey: process.env.googleMapsKey,
   },
-}
+};
